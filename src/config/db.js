049f4a926c54517cce76d084f37ef0bf231e1bd1
Juan Manuel Machado <juanmanuelmachadomaturana1@@gmail.com>
2025-08-11 10:55:12 -0500

    Conexion con base de datos

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+  }
+);
+
+sequelize.authenticate()
+  .then(() => console.log('✅ Conexión a MySQL exitosa'))
+  .catch(err => console.error('❌ Error de conexión:', err));
+
+module.exports = sequelize;
