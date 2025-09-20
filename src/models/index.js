843ad8183b23e64fcdfef4120a867829a8d2bc12
Yuver-10 <antoniodjromana@gmail.com>
2025-09-20 18:54:02 -0500

    fix: corregir rutas de importacion sensibles a mayusculas

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,6 @@ export { User, Rol } from "./user_rol.js";
 // Alias para compatibilidad: exportar como 'Role' (inglés)
 export { Rol as Role } from "./user_rol.js";
 // Exportar modelos de permisos, privilegios y tabla intermedia
-export { default as Permiso } from "./permiso.js";
-export { default as Privilegio } from "./privilegio.js";
+export { default as Permiso } from "./Permiso.js";
+export { default as Privilegio } from "./Privilegio.js";
 export { default as RolPermisoPrivilegio } from "./rol_permiso_privilegio.js";
